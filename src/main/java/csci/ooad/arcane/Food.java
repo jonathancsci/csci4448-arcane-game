19a79e48eb2d92b738543a64fe83636c12a0309b
Gamerscience10 <49415981+Gamerscience10@users.noreply.github.com>
2024-02-12 13:46:42 -0700

    implemented random names for entities and food

--- src/main/java/csci/ooad/arcane/Food.java
+++ src/main/java/csci/ooad/arcane/Food.java
@@ -1,10 +1,20 @@
 package csci.ooad.arcane;
 
+import java.util.Random;
+
 public class Food {
+    static String[] possibleNames = {"Carrot","Apple","Burger","Soup","Cheese Wheel","Steak","Cake","Ice Cream","Spaghetti"};
     private String name;
     private Integer healthRestored;
     public Food(String name, Integer healthRestored) {
+        Random randomNumberGenerator = new Random();
         this.name = name;
+        this.healthRestored = healthRestored;
+    }
+
+    public Food(Integer healthRestored) {
+        Random randomNumberGenerator = new Random();
+        this.name = possibleNames[randomNumberGenerator.nextInt(possibleNames.length)];
         this.healthRestored = healthRestored;
     }
 
