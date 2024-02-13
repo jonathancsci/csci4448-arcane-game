5aade8dcbbc849b2719bec0ef48facb49e1aef47
bluejaywu <155753916+bluejaywu@users.noreply.github.com>
2024-02-13 09:53:47 -0700

    100% test coverage for Creature

--- src/test/java/csci/ooad/arcane/CreatureTest.java
+++ src/test/java/csci/ooad/arcane/CreatureTest.java
@@ -2,54 +2,27 @@ package csci.ooad.arcane;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CreatureTest {
     @Test
-    public void gettersTest() {
-        Room room = new Room();
-        Creature testCreature = new Creature("Ogre", 5);
-        assertEquals("Ogre", testCreature.getName(), "Error in Creature.getName()");
-        assertEquals(5, testCreature.getHealth(), "Error in Creature.getHealth()");
-        assertNotNull(testCreature.getCurrentRoom(), "Error in Creature.getCurrentRoom()");
-    }
-
-    @Test
-    public void settersTest() {
-        Room room1 = new Room();
-        Room room2 = new Room();
-        Creature testCreature = new Creature("Ogre", 5);
-        testCreature.setName("Giant");
-        testCreature.setHealth(3);
-        testCreature.setCurrentRoom(room2);
-
-        assertEquals("Giant", testCreature.getName(), "Error in Creature.setName()");
-        assertEquals(3, testCreature.getHealth(), "Error in Creature.setHealth()");
-        assertEquals(room2, testCreature.getCurrentRoom(), "Error in Creature.setCurrentRoom");
-    }
-
-    @Test
-    public void rollDiceTest() {
-        Room room = new Room();
-        Creature testCreature = new Creature("Ogre", 5);
-        Integer diceRollResult = testCreature.rollDice();
-        assertTrue(diceRollResult >= 1 && diceRollResult <= 6, "Error in Creature.rollDice");
-    }
-
-    @Test
-    public void testIsOccupantOfCurrentRoom() {
-        Room room = new Room();
-        Creature testCreature = new Creature("Ogre", 5);
-        assertTrue(room.getOccupants().contains(testCreature));
+    public void testDefaultConstructor() {
+        String[] possibleNames = {"Cobblebeast","Dimcreeper","Ooze","Unruly Armor","Cavern Crab","Snapdragon","Wyvern"};
+        Creature testCreature = new Creature();
+        String creatureName = testCreature.getName();
+        Boolean isNameValid = Arrays.asList(possibleNames).contains(creatureName);
+        assertTrue(isNameValid, "Creature should have one of the predefined names");
     }
 
     @Test
     public void toStringTest() {
-        Room room = new Room();
         Creature testCreature = new Creature("Ogre", 5);
         assertEquals("Creature Ogre(health: 5)",testCreature.toString());
+        testCreature.setHealth(0);
+        assertEquals("Creature Ogre(health: 0); DEAD", testCreature.toString());
     }
 
 }
