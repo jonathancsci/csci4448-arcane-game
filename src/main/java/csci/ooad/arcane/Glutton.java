3019a9623211da4442749d1afb6aa75a9625a9fd
bluejaywu <155753916+bluejaywu@users.noreply.github.com>
2024-02-21 16:51:03 -0700

    Added Glutton

--- src/main/java/csci/ooad/arcane/Glutton.java
+++ src/main/java/csci/ooad/arcane/Glutton.java
@@ -1,4 +1,22 @@
 package csci.ooad.arcane;
 
-public class Glutton {
+public class Glutton extends Adventurer{
+    public Glutton() {
+        super(possibleNames, 3);
+    }
+
+    @Override
+    public void turn(Room currentRoom) {
+        Creature creature = currentRoom.getHealthiestCreature();
+        if (currentRoom.isThereFood()) {
+            Food food = currentRoom.takeFood();
+            Arcane.logger.info(this + " just ate a " + food.getName() + "\n");
+            eatFood(food);
+        } else if((creature != null) &&
+                (currentRoom.getHealthiestAdventurer() == this)) {
+            combat(creature);
+        } else {
+            moveRooms();
+        }
+    }
 }
