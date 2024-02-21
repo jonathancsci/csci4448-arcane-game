27d54424774021458c6ff78fd2eb6af0c62bd271
Gamerscience10 <49415981+Gamerscience10@users.noreply.github.com>
2024-02-21 13:23:01 -0700

    Moved Maze into class with Factory

--- src/main/java/csci/ooad/arcane/Maze.java
+++ src/main/java/csci/ooad/arcane/Maze.java
@@ -1,4 +1,154 @@
 package csci.ooad.arcane;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
 public class Maze {
+    Random randomNumberGenerator = new Random();
+    private Room[] rooms;
+    private ArrayList<Adventurer> adventurers = new ArrayList<>();
+    private ArrayList<Creature> creatures = new ArrayList<>();
+
+    public Maze() {
+
+    }
+
+    public void setRooms(Room[] rooms) {
+        this.rooms = rooms;
+    }
+
+    public Room getRoom(int num) {
+        Arcane.logger.debug(""+num);
+        if (num < 0 || num >= rooms.length) {
+            return null;
+        } else {
+            return rooms[num];
+        }
+    }
+
+    public int getSize() {
+        return rooms.length;
+    }
+
+    public Room getRandomRoom() {
+        return rooms[randomNumberGenerator.nextInt(rooms.length)];
+    }
+
+    public String toString() {
+        String status = "";
+        for(int i=0; i<rooms.length; i++) {
+            status += rooms[i];
+        }
+        return status;
+    }
+
+    public void addAdventurer(Adventurer adventurer) {
+        adventurers.add(adventurer);
+        getRandomRoom().addOccupant(adventurer);
+    }
+
+    public void addAdventurer(Adventurer adventurer, Room room) {
+        adventurers.add(adventurer);
+        room.addOccupant(adventurer);
+    }
+
+    public void addAdventurer(Adventurer[] adventurers) {
+        for(Adventurer a : adventurers) {
+            addAdventurer(a);
+        }
+    }
+
+    public void addCreature(Creature creature) {
+        creatures.add(creature);
+        getRandomRoom().addOccupant(creature);
+    }
+
+    public void addCreature(Creature creature, Room room) {
+        creatures.add(creature);
+        room.addOccupant(creature);
+    }
+
+    public void addCreature(Creature[] creatures) {
+        for(Creature c : creatures) {
+            addCreature(c);
+        }
+    }
+
+    public void addFood(Food food) {
+        getRandomRoom().addFood(food);
+    }
+
+    public void addFood(Food food, Room room) {
+        room.addFood(food);
+    }
+
+    public void addFood(Food[] food) {
+        for(Food f : food) {
+            addFood(f);
+        }
+    }
+
+    public void turn() {
+        Collections.sort(adventurers);
+        for(Adventurer adventurer : adventurers) {
+            if (adventurer.isDead()) {
+                continue;
+            }
+            Room currentRoom = adventurer.getCurrentRoom();
+            Creature creature = currentRoom.getHealthiestCreature();
+            if((creature != null) &&
+                    (currentRoom.getHealthiestAdventurer() == adventurer)) {
+                combat(adventurer, creature);
+            } else if ((creature == null) &&
+                    (currentRoom.isThereFood())) {
+                Food food = currentRoom.takeFood();
+                Arcane.logger.info(adventurer+" just ate a "+food.getName() + "\n");
+                adventurer.eatFood(food);
+                //dependency injection: the food is passed into the adventurer
+            } else {
+                adventurer.moveRooms();
+            }
+        }
+    }
+
+    public void combat(Adventurer combatantA, Creature combatantB) {
+        int rollA = combatantA.rollDice();
+        int rollB = combatantB.rollDice();
+        Arcane.logger.info(combatantA + " fought " + combatantB+ "\n");
+        if(rollA > rollB) {
+            Integer damageForCombatantB = rollA - rollB;
+            combatantB.takeDamage(damageForCombatantB);
+            if (combatantB.isDead()) {
+                Arcane.logger.info(combatantB + " was killed\n");
+            }
+            Arcane.logger.info(combatantB + " lost to " + combatantA+ "\n");
+        } else if (rollB > rollA) {
+            Integer damageForCombatantA = rollB - rollA;
+            combatantA.takeDamage(damageForCombatantA);
+            if (combatantB.isDead()) {
+                Arcane.logger.info(combatantA + " was killed\n");
+            }
+            Arcane.logger.info(combatantA + " lost to " + combatantB+ "\n");
+        }
+    }
+
+    public boolean checkAllCreaturesDead() {
+        for (int i = 0; i < creatures.size(); i++) {
+            if(!creatures.get(i).isDead()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkAllAdventurersDead() {
+        for (int i = 0; i < adventurers.size(); i++) {
+            if(!adventurers.get(i).isDead()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- src/main/java/csci/ooad/arcane/MazeFactory.java
+++ src/main/java/csci/ooad/arcane/MazeFactory.java
@@ -1,4 +1,107 @@
 package csci.ooad.arcane;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MazeFactory {
+
+    public void setGridRoomNames(Maze maze, int mazeWidth, int mazeHeight) {
+        for(int i=0; i<maze.getSize(); i++) {
+            String name = "";
+            if (i < mazeWidth) {
+                name = "North";
+            } else if (i >= mazeHeight*(mazeWidth-1)) {
+                name = "South";
+            }
+            if (i%mazeWidth == 0) {
+                name += "West";
+            } else if (i%mazeWidth == mazeWidth-1) {
+                name += "East";
+            }
+            if(name.equals("")) {
+                name = "Center";
+            }
+            maze.getRoom(i).setName(name);
+        }
+    }
+
+    public void autofillGridConnections(Maze maze, int mazeWidth, int mazeHeight) {
+        for(int x = 0; x < mazeWidth; x++) {
+            for(int y = 0; y < mazeHeight; y++) {
+                connectAdjacentRooms(maze,x,y,mazeWidth);
+            }
+        }
+    }
+
+    public void connectAdjacentRooms(Maze maze, int x, int y, int mazeWidth) {
+        Room currentRoom = roomAtCoords(maze, x, y, mazeWidth);
+        Room[] adjacentRooms = {roomAtCoords(maze, x - 1, y, mazeWidth), roomAtCoords(maze, x + 1, y, mazeWidth), roomAtCoords(maze, x, y - 1, mazeWidth), roomAtCoords(maze, x, y + 1, mazeWidth)};
+        for (int i = 0; i < adjacentRooms.length; i++) {
+            if (adjacentRooms[i] != null) {
+                currentRoom.addRoomConnection(adjacentRooms[i]);
+            }
+        }
+    }
+
+    public Room roomAtCoords(Maze maze, int x, int y, int mazeWidth) {
+        return maze.getRoom(mazeWidth*y+x);
+    }
+
+    public Maze gridRoomPrep(int mazeWidth, int mazeHeight) {
+        Maze maze = new Maze();
+        Room[] rooms = new Room[mazeWidth*mazeHeight];
+        for (int i = 0; i < rooms.length; i++) {
+            rooms[i] = new Room();
+        }
+        maze.setRooms(rooms);
+        setGridRoomNames(maze,mazeWidth,mazeHeight);
+        autofillGridConnections(maze,mazeWidth,mazeHeight);
+        return maze;
+    }
+
+    public Maze gridMazePrep(int mazeWidth, int mazeHeight, int advNum, int creaNum, int foodNum) {
+        Maze maze = gridRoomPrep(mazeWidth,mazeHeight);
+        maze.addAdventurer(generateAdventurers(advNum));
+        maze.addCreature(generateCreatures(creaNum));
+        maze.addFood(generateFood(foodNum));
+        return maze;
+    }
+
+    public Maze fourRoomGrid() {
+        return gridMazePrep(2, 2, 1, 1, 5);
+    }
+
+    public Maze nineRoomGrid() {
+        return gridMazePrep(3, 3, 2,2,10);
+    }
+
+    public Adventurer[] generateAdventurers(int num) {
+        Adventurer[] adventurers = new Adventurer[num];
+        for (int i = 0; i < num; i++) {
+            adventurers[i] = new Adventurer();
+        }
+        return adventurers;
+    }
+
+    public Creature[] generateCreatures(int num) {
+        Creature[] creatures = new Creature[num];
+        for (int i = 0; i < num; i++) {
+            creatures[i] = new Creature();
+        }
+        return creatures;
+    }
+
+    public Food[] generateFood(int num) {
+        Food[] foods = new Food[num];
+        for (int i = 0; i < num; i++) {
+            foods[i] = new Food();
+        }
+        return foods;
+    }
+
+
+    //For our example of polymorphism, the addAdventurer method is overloaded to allow for a multitude of adventurers to be added, or for the room to be specified in testing
+    //Kind of, I, Gavin, think of polymorphism as a bit of an umbrella term, a better example is in the toStrings
+
+
 }
