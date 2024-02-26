663c2822d894fae2af42b8bfdf6a17d4260c2fc1
Gamerscience10 <49415981+Gamerscience10@users.noreply.github.com>
2024-02-26 13:05:06 -0700

    Implemented argument parsing

--- src/main/java/csci/ooad/arcane/GameConfigurator.java
+++ src/main/java/csci/ooad/arcane/GameConfigurator.java
@@ -3,10 +3,25 @@ package csci.ooad.arcane;
 public class GameConfigurator {
     private static final MazeFactory mazeFactory= new MazeFactory();
     public static void main(String [] args) {
+        int adventurerNum = 2;
+        int creatureNum = 2;
+        int foodNum = 9;
+        int roomNum = 5;
         for (int i = 0; i < args.length; i++) {
-
+            if(args[i].equals("--numberOfAdventurers")) {
+                adventurerNum = Integer.parseInt(args[i+1]);
+            }
+            if(args[i].equals("--numberOfCreatures")) {
+                creatureNum = Integer.parseInt(args[i+1]);
+            }
+            if(args[i].equals("--numberOfFoodItems")) {
+                foodNum = Integer.parseInt(args[i+1]);
+            }
+            if(args[i].equals("--numberOfRooms")) {
+                roomNum = Integer.parseInt(args[i+1]);
+            }
         }
-        Arcane arcane = new Arcane(mazeFactory.buildFourRoomGrid());
+        Arcane arcane = new Arcane(mazeFactory.createMaze(roomNum,adventurerNum,creatureNum,foodNum));
         arcane.runGame();
     }
 }
