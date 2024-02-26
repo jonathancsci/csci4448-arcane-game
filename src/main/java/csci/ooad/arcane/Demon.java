3afc9eb2f99550de703898f916df2dbe4e46771c
bluejaywu <155753916+bluejaywu@users.noreply.github.com>
2024-02-26 11:20:09 -0700

    Added createDemon to CreatureFactory

--- src/main/java/csci/ooad/arcane/Demon.java
+++ src/main/java/csci/ooad/arcane/Demon.java
@@ -4,4 +4,8 @@ public class Demon extends Creature {
     public Demon() {
         super(possibleNames, 15);
     }
+
+    public Demon(String name, double health) {
+        super(name, health);
+    }
 }
