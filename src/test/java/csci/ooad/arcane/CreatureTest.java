1d11f43400a587594e7ff58ea51c281e5a05b26a
bluejaywu <155753916+bluejaywu@users.noreply.github.com>
2024-02-27 11:12:06 -0700

    100% coverage on CreateTest

--- src/test/java/csci/ooad/arcane/CreatureTest.java
+++ src/test/java/csci/ooad/arcane/CreatureTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CreatureTest {
     @Test
-    public void testDefaultConstructor() {
+    public void testConstructor() {
         String[] possibleNames = {
                 "Cobblebeast", "Dimcreeper", "Ooze", "Unruly Armor",
                 "Cavern Crab", "Snapdragon", "Wyvern"
@@ -23,9 +23,9 @@ public class CreatureTest {
     @Test
     public void toStringTest() {
         Creature testCreature = new Creature("Ogre", 5);
-        assertEquals("Creature Ogre(health: 5)", testCreature.toString());
+        assertEquals("Creature Ogre(health: 5.0)", testCreature.toString());
         testCreature.setHealth(0);
-        assertEquals("Creature Ogre(health: 0); DEAD", testCreature.toString());
+        assertEquals("Creature Ogre(health: 0.0); DEAD", testCreature.toString());
     }
 
 }
