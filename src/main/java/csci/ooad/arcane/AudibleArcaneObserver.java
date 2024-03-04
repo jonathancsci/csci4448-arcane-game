0e4c6c4a57893d79c2c2ea339a40c7d3f71d171f
bluejaywu <155753916+bluejaywu@users.noreply.github.com>
2024-03-04 16:42:28 -0700

    Added AudibleArcaneObserver

--- src/main/java/csci/ooad/arcane/AudibleArcaneObserver.java
+++ src/main/java/csci/ooad/arcane/AudibleArcaneObserver.java
@@ -1,19 +1,39 @@
 package csci.ooad.arcane;
 
-public class AudibleArcaneObserver extends IObserver {
-    private IObservable arcaneGame;
-    private Integer delayInSeconds = 5;
+import java.io.IOException;
+import java.util.List;
 
-    public void AudibleArcaneObserver(IObservable` arcaneGame) {
+public class AudibleArcaneObserver implements IObserver {
+    private IObservable arcaneGame;
+    private List<EventType> interestedEvents;
+    private Integer delayInSeconds = 3;
+
+    public void AudibleArcaneObserver(IObservable arcaneGame) {
         this.arcaneGame = arcaneGame;
+    }
+    public void AudibleArcaneObserver(IObservable arcaneGame, List<EventType> eventTypeList, Integer delayInSeconds) {
+        this.arcaneGame = arcaneGame;
+        this.interestedEvents = eventTypeList;
+        this.delayInSeconds = delayInSeconds;
     }
     public void AudibleArcaneObserver(IObservable arcaneGame, Integer delayInSeconds) {
         this.arcaneGame = arcaneGame;
+        this.interestedEvents = List.of(EventType.All);
         this.delayInSeconds = delayInSeconds;
     }
 
-    public void update(EventType eventDescription) {
-        String[] cmd = {"say", eventDescription};
-        Runtime.getRuntime().exec(cmd);
+    public void update(EventType eventType, String eventDescription) {
+        if (this.interestedEvents.contains(eventType) ||
+                        this.interestedEvents.contains(EventType.All)) {
+            try {
+                String[] cmd = {"say", eventDescription};
+                Runtime.getRuntime().exec(cmd);
+                Thread.sleep(delayInSeconds * 1000);
+            } catch (IOException e) {
+                throw new RuntimeException("Error in executing the say command", e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Error in Thread.sleep()", e);
+            }
+        }
     }
 }
