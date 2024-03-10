7b5142f2ec5d612e362e5fcbbc496c96151d6cd0
bluejaywu <155753916+bluejaywu@users.noreply.github.com>
2024-03-10 11:27:20 -0600

    attach audibleObserver to arcane

--- src/main/java/csci/ooad/arcane/GameConfigurator.java
+++ src/main/java/csci/ooad/arcane/GameConfigurator.java
@@ -3,6 +3,8 @@ package csci.ooad.arcane;
 import csci.ooad.layout.IMazeObserver;
 import csci.ooad.layout.MazeObserver;
 
+import java.util.List;
+
 public class GameConfigurator {
     public static void main(String [] args) {
         int adventurerNum = 2;
@@ -25,12 +27,15 @@ public class GameConfigurator {
         }
         Arcane arcane = new Arcane(MazeFactory.createMaze(roomNum,adventurerNum,creatureNum,foodNum));
 
-        int SECONDS_TO_PAUSE_BETWEEN_TURNS = 3000;
+        int SECONDS_TO_PAUSE_BETWEEN_TURNS = 5;
 
         IMazeObserver mazeObserver = MazeObserver.getNewBuilder("Arcane Game")
                 .useRadialLayoutStrategy()
                 .setDelayInSecondsAfterUpdate(SECONDS_TO_PAUSE_BETWEEN_TURNS)
                 .build();
+
+        AudibleArcaneObserver audibleObserver = new AudibleArcaneObserver(arcane, List.of(EventType.All), 5);
+        arcane.attach(audibleObserver);
 
         new MazeAdapter(arcane).attach(mazeObserver);
 
