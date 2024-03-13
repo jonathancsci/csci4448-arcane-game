31156dba2dca0fbdb3bc3d513e8c592aa35759de
bluejaywu <155753916+bluejaywu@users.noreply.github.com>
2024-03-13 13:49:02 -0600

    get rid of logger in eventbus

--- src/main/java/csci/ooad/arcane/EventBus.java
+++ src/main/java/csci/ooad/arcane/EventBus.java
@@ -3,8 +3,6 @@ package csci.ooad.arcane;
 import java.util.ArrayList;
 import java.util.List;
 
-import static csci.ooad.arcane.Arcane.logger;
-
 public class EventBus implements IObservable {
     private static EventBus eventBus;
     private List<IObserver> observerList = new ArrayList<>();
@@ -21,7 +19,6 @@ public class EventBus implements IObservable {
         this.observerList.add(observer);
     }
     public void notifyObservers(EventType postedEventType, String postedEventDescription) {
-        logger.info("I AM BEING OBSERVED BY "+observerList);
         for (IObserver observer : this.observerList) {
             observer.update(postedEventType, postedEventDescription);
         }
